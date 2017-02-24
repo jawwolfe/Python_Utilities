f40161fc421a866d8d3f08e8cb9f501cf6444dc6
Andrew Wolfe <jawwolfe@comcast.net>
2017-02-24 12:40:50 -0800

    Finish FTP backup script and hide password

--- backup.py
+++ backup.py
@@ -1,24 +1,25 @@
 from ftplib import FTP
 import zipfile
+import os
 import sys
+import glob
+import base64
 
 ########### MODIFY ########################
-
-USER = 'andrew01'
-PASS = ''
-
+USER = 'ftp_one'
+PASS_bytes = base64.b64decode(open('C:/utility.txt', 'r').read())
+PASS = PASS_bytes.decode("utf8")
 ########### MODIFY IF YOU WANT ############
-
 SERVER = 'www.daconsol.com'
 PORT = 21
 BINARY_STORE = True  # if False then line store (not valid for binary files (videos, music, photos...))
-
-
+###########################################
+path = 'C:\\backups\\'
+databases = ['DCS', 'Garden', 'Chameleon']
 ###########################################
 
 def print_line(result):
     print(result)
-
 
 def connect_ftp():
     # Connect to the server
@@ -27,7 +28,6 @@ def connect_ftp():
     ftp.login(USER, PASS)
     ftp.set_pasv(False)
     return ftp
-
 
 def upload_file(ftp_connetion, upload_file_path):
     # Open the file
@@ -52,13 +52,16 @@ def upload_file(ftp_connetion, upload_file_path):
     except IOError:
         print("No such file or directory... passing to next file")
 
-
 # Take all the files and upload all
 ftp_conn = connect_ftp()
 
-# for arg in sys.argv:
-#   upload_file(ftp_conn, arg)
-
-upload_file(ftp_conn, "C:/backups/Schedule.txt")
+for database in databases:
+    files = glob.glob(path + database + '\\*.BAK')
+    myfile = max(files , key = os.path.getctime)
+    myzip = myfile[:-4] + '.zip'
+    zip = zipfile.ZipFile(myzip, "w", zipfile.ZIP_DEFLATED)
+    zip.write (myfile)
+    zip.close()
+    upload_file(ftp_conn, myzip.replace('\\','/'))
 
 print("success")
